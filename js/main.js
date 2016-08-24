e6286cbec47460e9227e6acae01b77ff084ca473
rafaelescorcia23 <rafaelescorcia23@gmail.com>
2016-08-24 12:30:37 -0500

    Creando los items del listado de actividades
    dando el estilo a las actividades pero todavia sin los dos botones ni de
    eliminar ni de confirmar

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+document.getElementById('add').addEventListener('click', function(){
+
+    var value = document.getElementById('item').value;
+
+    if (value){
+
+      console.log(value);
+
+    }
+
+});
